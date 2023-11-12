cce875bf9d02c08b9bfc1da044e2933b59f3b7a3
Qolby <qolbyadha04@gmail.com>
2023-11-12 13:10:03 +0700

    Add Transaction model with associations and specs

---
+++ db/migrate/20231112060849_create_transactions.rb
@@ -0,0 +1,13 @@
+class CreateTransactions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :transactions do |t|
+      t.references :source_wallet, foreign_key: { to_table: :wallets }
+      t.references :target_wallet, foreign_key: { to_table: :wallets }
+      t.decimal :amount, precision: 10, scale: 2
+      t.string :transaction_type
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
