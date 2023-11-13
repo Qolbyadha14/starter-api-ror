cd132a0129024c9cb7534b5c11b35cf22a8f8737
Qolby <qolbyadha04@gmail.com>
2023-11-13 11:22:09 +0700

    Add spec Stock model

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Stock, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Stock do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:symbol) }
+    it { should validate_uniqueness_of(:symbol) }
+  end
 end
