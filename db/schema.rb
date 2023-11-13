0a804b405ce2c46c8431b55b8f249a7dac07723e
Qolby <qolbyadha04@gmail.com>
2023-11-13 19:56:46 +0700

    Add TeamsController with CRUD actions and routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_034202) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_13_124104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_034202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["wallets_id"], name: "index_team_wallets_on_wallets_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "transactions", force: :cascade do |t|
