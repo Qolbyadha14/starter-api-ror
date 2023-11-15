32d39857b13a7eec0d7cb9e7ea91dd8c92e2fed9
Qolby <qolbyadha04@gmail.com>
2023-11-15 20:52:22 +0700

    Add authentication to StocksController and add endpoints for teams and wallets

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,7 @@
 # app/controllers/stocks_controller.rb
 class StocksController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     stock_price = LatestStockPrice.price_all
     render json: { data: stock_price }, status: :ok
